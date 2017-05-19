f67e3dcdf13769bf4e892cf93af0217f4369aa68
Pablo Puga <pablo.puga.git@gmail.com>
2017-05-19 11:42:20 +0200

    Added a publish script

---
+++ publish.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ $(git status -s) ]]; then
+    echo "There are changes without saving. Save (commit) them or discard (reset --hard) them before publishing"
+    exit 1;
+fi
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# We try to download the latest changes made by our workmates
+cd public
+git rebase
+cd -
+
+# Build the project.
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="New Publish [`date +'%d/%m/%Y %H:%M'`]"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin gh-pages
+
+# Come Back
+cd ..
